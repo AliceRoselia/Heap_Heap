0f9988cc62f40e1b630f7bbe9f6fdff37d0506e8
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-08 05:49:10 +0700

    Update Main.jl

--- Main.jl
+++ Main.jl
@@ -1,9 +1,21 @@
+#= 
+This is an implementation of a priority_queue data structure, customized for modern cpu architectures
+
+
+Special Thanks to Gandalf and other people in the Hoj discord server for helping me out.
 
 
 
-
+=#
 
 module Heap_Heap
+
+
+struct Heap_Heap{base_size, layer}
+
+end
+
+
 
 
 
