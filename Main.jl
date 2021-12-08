542329918614e44fad456289f6078e32ff35f739
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-08 08:31:28 +0700

    Update Main.jl

--- Main.jl
+++ Main.jl
@@ -16,23 +16,23 @@ mutable struct Heap_Heap{base_size, layer, dtype}
     base_array::Union{Vector{dtype}, Nothing}
     summary::Union{Heap_Heap{base_size, layer-1, dtype}, Nothing}
     data::Union{Vector{Heap_Heap{base_size, layer-1, dtype, Nothing}}}
-    @inline function Heap_Heap{base_size, layer, dtype}()
+    @inline function Heap_Heap{base_size, layer, dtype}(;In_layer=layer)
         #I will change this to generated function later if the compiler fails to optimize away the branches
         x = new()
-        if layer == 0
+        if In_layer == 0
 
             x.base_array = Vector{dtype}()
             x.summary = Nothing
             x.data = Nothing
         else
             x.base_array = Nothing
-            x.summary = new{base_size, layer-1, dtype}()
+            x.summary = new(;In_layer = In_layer-1, dtype = dtype)
             
         end
     end
 end
 
-
+Heap_Heap(;base_size=32, In_layer = 2, dtype::Type) = Heap_Heap{base_size, In_layer, dtype}()
 
 
 
