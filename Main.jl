61f42507b21cf035c698b60ed82d062e85a17ae9
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-08 08:55:25 +0700

    Update Main.jl

--- Main.jl
+++ Main.jl
@@ -8,30 +8,14 @@ Special Thanks to Gandalf and other people in the Hoj discord server for helping
 
 =#
 
-module My_Heap_Heap
 
-mutable struct Heap_Heap{base_size, layer, dtype}
-    base_array::Union{Vector{dtype}, Nothing}
-    summary::Union{Heap_Heap{base_size, layer-1, dtype}, Nothing}
-    data::Union{Vector{Heap_Heap{base_size, layer-1, dtype, Nothing}}}
-    @inline function Heap_Heap(; base_size, In_layer, dtype)
-        #I will change this to generated function later if the compiler fails to optimize away the branches
-        x = new{base_size, In_layer-1, dtype}(;base_size = base_size, In_layer = In_layer-1, dtype = dtype)
-        if In_layer == 0
+mutable struct Heap_Heap{Dtype}
+    base_array::Union{Vector{Dtype}, Nothing}
+    summary::Union{Heap_Heap{Base_size, Layer-1, Dtype}, Nothing}
+    data::Union{Vector{Heap_Heap{Base_size, Layer-1, Dtype, Nothing}}}
 
-            x.base_array = Vector{dtype}()
-            x.summary = Nothing
-            x.data = Nothing
-        else
-            x.base_array = Nothing
-            x.summary = new{base_size, In_layer-1, dtype}(;In_layer = In_layer-1, dtype = dtype)
-            
-        end
-    end
 end
 
 Heap_Heap(;base_size=32, In_layer = 2, dtype::Type) = Heap_Heap{base_size, In_layer, dtype}()
 
 
-
-end
