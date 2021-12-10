e21e81c3feba79f005f0d62859d15d017da49796
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-10 09:01:32 +0700

    Update Test.jl

--- Test.jl
+++ Test.jl
@@ -1,2 +1,19 @@
 #This is temporary for test purpose.
 #You should probably ignore it unless you're developing.
+
+
+
+
+function binsearch_insert(X, data, cmp = <)
+    mindex = 1
+    maxdex = X.size
+    while mindex < maxdex
+        middex = (mindex+maxdex)>>1
+        if cmp(content, X.data[middex])
+            maxdex = middex
+        else
+            mindex = middex+1
+        end
+    end
+    
+end
