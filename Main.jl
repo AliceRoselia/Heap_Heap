e1ecf4bfa4c679e82e9437e495cd447169387ff7
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-11 07:18:52 +0700

    Update Main.jl

--- Main.jl
+++ Main.jl
@@ -55,7 +55,14 @@ end
 
 #TODO: fix this.
 #Make the function definition works.
-Heap_Heap_type(Dtype, x::Integer) = x==0 ? Base_layer{Dtype} : Heap_layer{Heap_Heap_type(Dtype, x-1), Heap_Heap_type(summary_key{Dtype}, x-1)}
+#Heap_Heap_type(Dtype, x::Integer) = x==0 ? Base_layer{Dtype} : Heap_layer{Heap_Heap_type(Dtype, x-1), Heap_Heap_type(summary_key{Dtype}, x-1)}
+
+function Heap_Heap_type(Dtype::Type, x::Integer, cmp::Function)
+    if x==0
+        return Base_layer{Dtype, cmp}
+    else
+        @inline cmp_key(a,b) = cmp(a.data, b.data)
+        return Heap_layer{Heap_Heap_type(Dtype, x-1, cmp), Heap_Heap_type(summary_key{Dtype}, x-1, cmp_key)}
 
 
 
