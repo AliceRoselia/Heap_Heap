2a99779682234c60b3e4879858ed7fb1b153ef6f
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-20 16:17:51 +0700

    Update Test.jl

--- Test.jl
+++ Test.jl
@@ -4,30 +4,16 @@
 using Main
 include("Main.jl")
 
-A = Base_layer{Int, 32, <}()
-push!(A,99)
-replace!(A,101)
-display(A)
-push!(A, 25)
-replace!(A,105)
-display(A)
-push!(A,47)
-push!(A,1)
-push!(A,29)
-push!(A,3)
-push!(A,152)
-display(A)
-println(replace!(A,37))
-display(A)
-println(replace!(A,99))
-println(replace!(A,-1))
-display(A)
-println(replace!(A,-3))
-println(replace!(A, 77))
-display(A)
+
 
 Fst_layer_type = Heap_Heap_type(Int, 2, <)
 
 A = Fst_layer_type()
 
 display(A.data)
+
+
+for i in 1:35
+    push!(A,i)
+end
+display(A)
