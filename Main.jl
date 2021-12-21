76ad56ef685479a350fecac0bf1b897b4c829632
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-21 20:45:26 +0700

    Update Main.jl

--- Main.jl
+++ Main.jl
@@ -181,8 +181,6 @@ end
     cutoff = div(Base_size, 2)
     new_one.size = Base_size-cutoff
     new_one.data[1:new_one.size] = X.data[cutoff+1:end]
-    println(X.data)
-    println(new_one.data)
     X.size = cutoff
     return new_one
 end
