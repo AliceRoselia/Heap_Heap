2e5c10ef49f0e6664a94f5a8ef99033b9ca0927d
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2021-12-21 21:05:34 +0700

    Update Main.jl

--- Main.jl
+++ Main.jl
@@ -237,6 +237,7 @@ end
         #println("Working in progress")
         #Todo: make sure the data stays in place. 
     else
+        println(X.summary)
         replace!(X.summary, summary_key(sub_heap, peek(sub_heap)))
     end
     return out
